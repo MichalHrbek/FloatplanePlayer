fb947e69188bb14742afab4f304b22207e5e5bbd
LosSalmonos <example@example.com>
2021-11-14 09:11:37 +0100

    Web player
    Updated to not require video player installed instead redirects to web player

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 
 namespace FpV
@@ -10,7 +11,7 @@ namespace FpV
             string creatorId = "59f94c0bdd241b70349eb72b";
             int maxVid = 20;
             string searchPhrase = "";
-            string pathToPlayer = @"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe";
+            string pathToPlayer = @"https://michalhrbek.github.io/Projects/Floatplane/Player";
             int resolution = 1080; // 480, 720, 1080, 2160(4K), 4320(8K)
 
             List<Post> Posts = Get.getVideoList(creatorId, maxVid, searchPhrase, 1);
@@ -27,8 +28,12 @@ namespace FpV
             int num = int.Parse(Console.ReadLine());
             if (num > 0 && num <= Posts.Count)
             {
-                System.Diagnostics.Process.Start(pathToPlayer, "https://edge01-na.floatplane.com/Videos/" + Posts[num-1].attachmentOrder[0] + "/" + resolution + ".mp4");
-                Console.WriteLine("https://edge01-na.floatplane.com/Videos/" + Posts[num-1].attachmentOrder[0] + "/" + resolution + ".mp4");
+                //Console.WriteLine("https://edge01-na.floatplane.com/Videos/" + Posts[num-1].attachmentOrder[0] + "/" + resolution + ".mp4");
+                var uri = pathToPlayer + "?id=" + Posts[num - 1].attachmentOrder[0] + "&res=" + resolution;
+                var psi = new System.Diagnostics.ProcessStartInfo();
+                psi.UseShellExecute = true;
+                psi.FileName = uri;
+                System.Diagnostics.Process.Start(psi);
             }
             else
             {
